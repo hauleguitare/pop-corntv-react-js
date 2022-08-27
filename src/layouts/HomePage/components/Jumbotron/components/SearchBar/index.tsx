17cf193d0a874185a2c215388ca487dccb66011e
hauleguitare <alexander.dutyone@gmail.com>
2022-08-27 23:40:49 +0700

    Update 27.8.22

---
+++ src/layouts/HomePage/components/Jumbotron/components/SearchBar/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import {AiOutlineClose} from 'react-icons/ai';
+
+
+const SeacrhBarComponent: React.FunctionComponent = () => {
+    const [isShowSearchBar, setShowSearchBar] = useState<boolean>(false);
+
+    const handleOnClickSearch = () => {
+        setShowSearchBar(!isShowSearchBar);
+    }
+
+    useEffect(() => {
+        const FindElementSearch = () =>{
+            try{
+                const ElementSearch = document.getElementById('search_bar_input');
+                if (ElementSearch !== null)
+                {
+                    ElementSearch.classList.toggle('active_search_bar_input');
+                }
+            }catch(e)
+            {
+                console.log('ERROR: ',e);
+            }
+        };
+        FindElementSearch();
+    }, [isShowSearchBar]);
+
+  return (
+    <div className='flex pt-4'>
+        <div id='search_bar_input' className='relative flex w-[64px] h-[48px] sm:h-[64px] rounded-full bg-white shadow-lg duration-500'>
+            <button id='icon_search_bar_btn' className='absolute top-0 bottom-0 left-4 z-20 duration-300' onClick={handleOnClickSearch}>
+                {isShowSearchBar === false ? (<BsSearch size={'30px'}/>) : (<AiOutlineClose size={'30px'}/>)}
+            </button>
+            <form id='show_form_search_bar' className='hidden w-full relative z-10'>
+                <label htmlFor='search'>
+                    <input type="text" className='w-full h-full rounded-full pl-16 pr-20 text-sm sm:text-xl' placeholder='search more movies...'/>
+                </label>
+                <button id='btn_search_bar_movies' className='hidden absolute top-0 right-0 bottom-full w-[84px] h-full rounded-full group 
+                        hover:from-slate-200 hover:to-neutral-200 bg-gradient-to-tr from-slate-800 to-stone-700 transition-colors ease-out duration-300'>
+                    <span className='font-["Merriweather"] text-white group-hover:text-black '>SEARCH</span>
+                </button>
+            </form>
+        </div>
+    </div>
+   );
+};
+
+export default SeacrhBarComponent;
--- src/layouts/HomePage/components/Jumbotron/index.tsx
+++ src/layouts/HomePage/components/Jumbotron/index.tsx
@@ -1,27 +1,19 @@
 import * as React from 'react';
 import { BsSearch } from 'react-icons/bs';
 import { IMAGE_BACKGROUND } from '../../../../images';
+import SeacrhBarComponent from './components/SearchBar';
 
 
 
 const JumbotronComponent: React.FunctionComponent = () => {
   return (
     <div className='h-full w-full relative text-black'>
-        <img src={IMAGE_BACKGROUND} className='h-full w-full'/>
+        <img src={'https://i.picsum.photos/id/1005/1280/384.jpg?blur=5&hmac=txA53Shl-EU-sxvJXaEYiwzrRMVmL3u_5AYe9PJ_7-w'} className='h-full w-full object-cover'/>
         <div className='fixed flex flex-col justify-center inset-0 h-[384px] w-auto'>
         <div className='flex flex-col px-8'>
-            <h5 className='text-[80px] font-["Playfair_Display"] font-semibold italic'>Welcome</h5>
-            <span className='font-["Playfair_Display"] font-medium text-[20px]'>Millions of movies, TV shows and people to discover. Explore now.</span>
-            <div className='flex items-center justify-end w-full pt-8 relative'>
-            <button className="fixed mr-4 mb-1" type='submit'>
-                <BsSearch size={'25px'}/>
-            </button>
-            <form action="/search" className='w-full h-10'>
-                <label htmlFor='search'>
-                <input className='rounded-full w-full h-10 md:text-lg text-[13px] px-4 py-2 font-["Lora"] text-gray-600' placeholder='Seacrh for movies, TV shows and more...'/>
-                </label>
-            </form>
-            </div>
+          <h5 className='text-[80px] font-["Playfair_Display"] font-semibold italic'>Welcome</h5>
+          <span className='font-["Playfair_Display"] font-medium text-[20px]'>Millions of movies, TV shows and people to discover. Explore now.</span>
+          <SeacrhBarComponent/>
         </div>
         </div>
     </div>
